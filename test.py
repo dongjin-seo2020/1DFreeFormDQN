e8bf4927b2b0a1294030f65e04c055bff401f1ac
Dongjin Seo <65718383+dongjin-seo2020@users.noreply.github.com>
2021-02-26 00:02:23 -0800

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,11 @@ import os
 import subprocess
 
 n_cells=64
-struct = str(np.ones(n_cells, dtype=int))
+struct = str(np.ones(n_cells))
 print(struct)
 wavelength = str(900)
 desired_angle=str(60)
+subprocess.Popen('chmod +x ' + os.getcwd()+ '/solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/Eval_Eff_1D', shell=True)
 subprocess.Popen('chmod +x ' + os.getcwd()+ '/solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh', shell=True)
 effs = subprocess.Popen("./solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh /usr/local/MATLAB/MATLAB_Runtime/v98 struct wavelength desired_angle", shell=True)
 
