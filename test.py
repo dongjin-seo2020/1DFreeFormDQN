c234bfc01b53c05d99922bf80ecbcbec43e34d02
Dongjin Seo <65718383+dongjin-seo2020@users.noreply.github.com>
2021-03-01 20:14:05 -0800

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ struct[0] = -1
 print(struct)
 wavelength = str(900)
 desired_angle=str(60)
-subprocess.Popen('chmod +x ' + os.getcwd()+ '/solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/Eval_Eff_1D', shell=True)
-subprocess.Popen('chmod +x ' + os.getcwd()+ '/solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh', shell=True)
+subprocess.Popen('chmod +x ' + os.getcwd()+ '/solvers/Eval_Eff_1D/for_redistribution_files_only/Eval_Eff_1D', shell=True)
+subprocess.Popen('chmod +x ' + os.getcwd()+ '/solvers/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh', shell=True)
 # effs = subprocess.Popen("./solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh /usr/local/MATLAB/MATLAB_Runtime/v98 "+struct+' '+wavelength+' '+desired_angle, shell=True)
-effs = subprocess.call(["./solvers/Eval_Eff_1D/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh", "/usr/local/MATLAB/MATLAB_Runtime/v98", struct, wavelength, desired_angle])
+effs = subprocess.call(["./solvers/Eval_Eff_1D/for_redistribution_files_only/run_Eval_Eff_1D.sh", "/usr/local/MATLAB/MATLAB_Runtime/v98", struct, wavelength, desired_angle])
 
 print('effs: ', effs)
 # subprocess.run('chmod +x ./solvers/Eval_Eff_1D/Eval_Eff_1D/for_testing/run_Eval_Eff_1D.sh')
