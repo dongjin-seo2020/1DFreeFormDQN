128a55feb48c4a564b5d6b4ac680816f7749fc48
Dongjin Seo <65718383+dongjin-seo2020@users.noreply.github.com>
2021-04-14 00:17:33 -0700

    Update deflector_reticolo.py

--- deflector_reticolo.py
+++ deflector_reticolo.py
@@ -45,7 +45,7 @@ class CustomEnv(gym.Env):
         else:
             raise ValueError('struct component should be 1 or -1')
         key = tuple(struct_after.to_list())
-        if key is in self.eff_table:
+        if key in self.eff_table:
             self.eff = self.eff_table[key]
         else:
             self.eff = self.getEffofStructure(matlab.double(struct_after.tolist()), self.wavelength,\
