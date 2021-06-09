0baf5976f6f7cac970a2a040ec7b837481fa553f
Dongjin Seo <65718383+dongjin-seo2020@users.noreply.github.com>
2021-06-09 00:45:06 -0700

    Update replaybuffer.py

--- replaybuffer.py
+++ replaybuffer.py
@@ -33,39 +33,3 @@ class ReplayBuffer():
 
     def size(self):
         return len(self.buffer)
-
-#written by namdw
-class ReplayBuffer_():
-    def __init__(self, buffer_limit, n_cells=64):
-        self.s = np.ones((buffer_limit, n_cells), dtype=np.uint8)
-        self.a = np.ones((buffer_limit, 1), dtype=np.uint8)
-        self.r = np.ones((buffer_limit, 1), dtype=np.uint8)
-        self.d = np.ones((buffer_limit, 1), dtype=np.uint8)
-        self.s_ = np.ones((buffer_limit, n_cells), dtype=np.uint8)
-        
-        self.max_size = buffer_limit
-        self.curr_idx = 0
-        self.curr_size = 0
-
-    def put(self, transition):
-        s, a, r, d, s_ = transition
-        self.s[self.curr_idx] = s
-        self.a[self.curr_idx] = a
-        self.r[self.curr_idx] = r
-        self.d[self.curr_idx] = d
-        self.s_[self.curr_idx] = s_
-        self.curr_size = min(self.curr_size + 1, self.max_size)
-        self.curr_idx = (self.curr_idx + 1) % self.max_size
-
-    def sample(self, n):
-        random_idx = np.random.randint(self.curr_size, size=n)
-        s = torch.from_numpy(self.s[random_idx]).float()
-        a = torch.from_numpy(self.a[random_idx]).long()
-        r = torch.from_numpy(self.r[random_idx]).float()
-        d = torch.from_numpy(self.d[random_idx]).float()
-        s_ = torch.from_numpy(self.s_[random_idx]).float()
-        
-        return (s, a, r, d, s_)
-
-    def size(self):
-        return self.curr_size
